7eb4b477cbb41493b4f7aff17a463eee71ad9086
Anton Silenin <anton.silenin@gmail.com>
2018-08-27 22:35:00 +0300

    more script to the god of script

---
+++ supreme3/index.js
@@ -0,0 +1,28 @@
+// var sayHello = function (extName) {
+//     name = prompt("твоё имя, кожанный мешок?");
+//     if (name = "") {
+//         alert("введи имя, я сказал");
+//         sayHello(extName);
+//     } else {
+//         if (name == extName) {
+//             alert("Ок")
+//         } else {
+//             alert("не ври мне, " + extName)
+//         };
+//     };
+// }
+
+// sayHello('Мила');
+
+// var sumNumbers = function () {
+//     alert(2 + 2);
+// }
+
+// sumNumbers();
+
+// console.log ("привет из консоли");
+
+var sumNumbers = function (a, b) {
+    var a = a+b;
+    console.log (a);
+}
